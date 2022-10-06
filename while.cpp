0bfc4ddd0a78f14840fb04e36d6bdb4551de878b
ruthmwende19 <114397310+ruthmwende19@users.noreply.github.com>
2022-10-06 10:43:34 +0300

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,18 @@
+// C++ Program to print numbers from 15 to 20
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int j= 15;
+ 
+  int i=1;
+   while(j<=20){
+       cout<<j<<" ";
+     ++j;
+       
+   }
+    
+  return 0; 
+}
