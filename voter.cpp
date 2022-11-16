cf36833ad1bfa5c2ec38d3f69ca6c28ade3aff14
ruthmwende19 <114397310+ruthmwende19@users.noreply.github.com>
2022-11-16 22:49:12 +0300

    Add files via upload

---
+++ voter.cpp
@@ -0,0 +1,18 @@
+// program to check voter eligibility 
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    if(age>=18)
+    {
+        cout<<"eligible to vote"<<endl;
+    }
+    else
+    {
+        cout<<"not eligible to vote";
+    }
+    return 0;
+}
